3c4a9f4ff631d7c8cd15f37804210e0b42397ea6
Jayesh-Gautam <78686495+Jayesh-Gautam@users.noreply.github.com>
2024-11-28 22:22:07 +0530

    Delete Datamatics_techfest-main directory

--- Datamatics_techfest-main/clean_history.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-git filter-branch --force --index-filter \
-  "git rm --cached --ignore-unmatch Default.xaml Gmail_Email_Fetch_Extract/Default.xaml" \
-  --prune-empty --tag-name-filter cat -- --all
-
-git push origin --force --all
